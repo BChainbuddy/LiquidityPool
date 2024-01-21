502cb9faebb6df42fdc7dac020c15454c8e8b10e
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-01-21 23:13:40 +0100

    updates to PoolTracker

--- contracts/PoolTracker.sol
+++ contracts/PoolTracker.sol
@@ -18,8 +18,16 @@ contract PoolTracker {
     // Mapping of pool Paris, to store existing ones
     mapping(address => address) public poolPairs;
 
-    // Pool creator
-    function createPool(address _assetOneAddress, address _assetTwoAddress) external {
+    // Mapping of pool per Owner
+    mapping(address => LiquidityPool[]) public poolOwner;
+
+    // Pool creator, approve enough for two transferfroms(one to contract(by msg sender) and one from contract(by contract))
+    function createPool(
+        address _assetOneAddress,
+        address _assetTwoAddress,
+        uint256 amountOne,
+        uint256 amountTwo
+    ) external {
         if (
             poolPairs[_assetOneAddress] == _assetTwoAddress ||
             poolPairs[_assetTwoAddress] == _assetOneAddress
@@ -27,15 +35,20 @@ contract PoolTracker {
         {
             revert PoolTracker_pairAlreadyExists();
         }
-        if (
-            IERC20(_assetOneAddress).balanceOf(msg.sender) < 100000000000 ||
-            IERC20(_assetOneAddress).balanceOf(msg.sender) < 100000000000
-        ) // To prevent creation of pool if owner doesnt have tokens
-        {
-            revert PoolTracker_noTokensDetected();
-        }
+        // Transfer of tokens
+        IERC20(_assetOneAddress).transferFrom(msg.sender, address(this), amountOne);
+        IERC20(_assetTwoAddress).transferFrom(msg.sender, address(this), amountTwo);
+        // Creation of pool
         LiquidityPool poolAddress = new LiquidityPool(_assetOneAddress, _assetTwoAddress);
+        // Approve
+        IERC20(_assetOneAddress).approve(address(poolAddress), amountOne);
+        IERC20(_assetTwoAddress).approve(address(poolAddress), amountTwo);
+        // Add initial liquidity
+        poolAddress.addInitialLiquidity(amountOne, amountTwo);
+        // Update mappings
+        poolOwner[msg.sender].push(poolAddress);
         poolPairs[_assetOneAddress] = _assetTwoAddress;
+        // Emit the event
         emit poolCreated(poolAddress, _assetOneAddress, _assetTwoAddress);
     }
 }
--- test/PoolTracker.test.js
+++ test/PoolTracker.test.js
@@ -3,8 +3,10 @@ const { developmentChains } = require("../helper-hardhat-config")
 const { assert, expect } = require("chai")
 
 describe("Pool tracker test", () => {
-    let poolTracker, deployer, token1, token2
+    let poolTracker, deployer, token1, token2, mintAmount, approveAmount
     beforeEach(async () => {
+        mintAmount = ethers.parseEther("1000")
+        approveAmount = ethers.parseEther("5000")
         await deployments.fixture(["all"])
         deployer = (await getNamedAccounts()).deployer
         token1 = await ethers.getContract("SimpleToken", deployer)
@@ -13,33 +15,85 @@ describe("Pool tracker test", () => {
     })
     describe("Creates a pool", () => {
         it("adds Pool to mapping", async () => {
-            await poolTracker.createPool(token1.target, token2.target)
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            await poolTracker.createPool(token1.target, token2.target, mintAmount, mintAmount)
             const array = await poolTracker.poolOwner(deployer, 0)
             expect(array).to.not.equal(undefined)
             await expect(poolTracker.poolOwner(deployer, 1)).to.be.reverted
         })
         it("emits the event", async () => {
-            const transaction = await poolTracker.createPool(token1.target, token2.target)
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            const transaction = await poolTracker.createPool(
+                token1.target,
+                token2.target,
+                mintAmount,
+                mintAmount
+            )
             const txReceipt = await transaction.wait(1)
             const array = await poolTracker.poolOwner(deployer, 0)
-            expect(txReceipt.logs[0].args.pool).to.equal(array)
-            expect(txReceipt.logs[0].args.assetOne).to.equal(token1.target)
-            expect(txReceipt.logs[0].args.assetTwo).to.equal(token2.target)
+            expect(txReceipt.logs[11].args.pool).to.equal(array)
+            expect(txReceipt.logs[11].args.assetOne).to.equal(token1.target)
+            expect(txReceipt.logs[11].args.assetTwo).to.equal(token2.target)
         })
         it("Enables liquidity Pool functionalities", async () => {
-            const transaction = await poolTracker.createPool(token1.target, token2.target)
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            const transaction = await poolTracker.createPool(
+                token1.target,
+                token2.target,
+                mintAmount,
+                mintAmount
+            )
             const txReceipt = await transaction.wait(1)
-            const poolAddress = txReceipt.logs[0].args.pool
+            const poolAddress = txReceipt.logs[11].args.pool
             const poolContract = await ethers.getContractAt("LiquidityPool", poolAddress)
             expect(await poolContract.assetOneAddress()).to.equal(token1.target)
             expect(await poolContract.assetTwoAddress()).to.equal(token2.target)
         })
         it("Sets the deployer as the owner of the liquidity pool", async () => {
-            const transaction = await poolTracker.createPool(token1.target, token2.target)
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            const transaction = await poolTracker.createPool(
+                token1.target,
+                token2.target,
+                mintAmount,
+                mintAmount
+            )
             const txReceipt = await transaction.wait(1)
-            const poolAddress = txReceipt.logs[0].args.pool
+            const poolAddress = txReceipt.logs[11].args.pool
             const poolContract = await ethers.getContractAt("LiquidityPool", poolAddress)
             expect(await poolContract.owner()).to.equal(poolTracker.target)
+        })
+        it("Sets the pool pair", async () => {
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            const transaction = await poolTracker.createPool(
+                token1.target,
+                token2.target,
+                mintAmount,
+                mintAmount
+            )
+            await transaction.wait(1)
+            expect(await poolTracker.poolPairs(token1.target)).to.equal(token2.target)
+        })
+        it("Revert if pool pair exists", async () => {
+            await token1.approve(poolTracker.target, approveAmount)
+            await token2.approve(poolTracker.target, approveAmount)
+            const transaction = await poolTracker.createPool(
+                token1.target,
+                token2.target,
+                mintAmount,
+                mintAmount
+            )
+            await transaction.wait(1)
+            await expect(
+                poolTracker.createPool(token1.target, token2.target, mintAmount, mintAmount)
+            ).to.be.reverted
+            await expect(
+                poolTracker.createPool(token2.target, token1.target, mintAmount, mintAmount)
+            ).to.be.reverted
         })
     })
 })
